aa18a33920df895316bf1f94dc54ebb742cf03d8
Yu Shi <123338811+yshi91@users.noreply.github.com>
2023-02-17 02:13:35 -0800

    Update app.py

--- app.py
+++ app.py
@@ -10,95 +10,95 @@ from matplotlib.figure import Figure
 
 import io
 import base64
+import sqlite3
 
 ### stuff from last class
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-    return render_template('main_better.html')
+def base():
+    return render_template('base.html')
 
-@app.route('/ask/', methods=['POST', 'GET'])
-def ask():
-    if request.method == 'GET':
-        return render_template('ask.html')
-    else:
-        try:
-            return render_template('ask.html', name=request.form['name'], student=request.form['student'])
-        except:
-            return render_template('ask.html')
-#######
-
-####### group exercise
-@app.route('/hello/')
-def hello():
-    return render_template('hello.html')
-
-@app.route('/hello/<name>')
-def hello_name(name):
-    return render_template('hello.html', name=name)
 
 #######
-# Request object: https://flask.palletsprojects.com/en/2.1.x/api/#flask.Request
-@app.route('/submit-basic/', methods=['POST', 'GET'])
-def submit_basic():
-    if request.method == 'GET':
-        return render_template('submit-basic.html')
-    else:
-        try:
-            # this is how you can access the uploaded file
-            # img = request.files['image']
-            return render_template('submit-basic.html', thanks=True)
-        except:
-            return render_template('submit-basic.html', error=True)
-
-
-# matplotlib: https://matplotlib.org/3.5.0/gallery/user_interfaces/web_application_server_sgskip.html
-# plotly: https://towardsdatascience.com/web-visualization-with-plotly-and-flask-3660abf9c946
-@app.route('/submit-advanced/', methods=['POST', 'GET'])
+@app.route('/submit/', methods=['POST', 'GET'])
 def submit():
     if request.method == 'GET':
         return render_template('submit.html')
     else:
-        try:            
-            '''
-            1. Access the image
-            2. Load the pickled ML model
-            3. Run the ML model on the image
-            4. Store the ML model's prediction in some Python variable
-            5. Show the image on the template
-            6. Print the prediction and some message on the template
-            '''
-            # 1
-            img = request.files['image'] # file object 144.txt 
-            img = np.loadtxt(img) # numpy array with the pixel values
+        message, handle = insert_message(request)
+        return render_template('submit.html', thanks = True, handle = handle, message = message)
 
-            x = img.reshape(1, 64)
-            
-            # 2
-            model = pickle.load(open('mnist-model/model.pkl', 'rb'))
-            
-            # 3, 4
-            digit = model.predict(x)[0]
 
-            # 5 
-            fig = Figure(figsize=(3, 3))
-            ax = fig.add_subplot(1, 1, 1,)
-            ax.imshow(img, cmap='binary')
-            ax.axis("off")
+# matplotlib: https://matplotlib.org/3.5.0/gallery/user_interfaces/web_application_server_sgskip.html
+# plotly: https://towardsdatascience.com/web-visualization-with-plotly-and-flask-3660abf9c946
+@app.route('/view/')
+def view():
+    messages = random_messages(3)
+    return render_template('view.html', messages = messages)
+    
 
-            # weird part of 5
-            pngImage = io.BytesIO()
-            FigureCanvas(fig).print_png(pngImage) # convert the pyplot figure object to a PNG image
+        
+def get_message_db():
+    # use the try except command to test if there is a database called message_db in the g attribute of the app
+    # if there is, we just return the database; if there is not, go to the except
+    try:
+        return g.message_db
+    
+    #if there is no such database in g, we just connect one in the attribute g
+    except:
+        g.message_db = sqlite3.connect("messages_db.sqlite")
+        
+        # use the SQL command to check if there is a table called messages in the database
+        # if there is no such table, we create one in the database
+        # with three columns id, handle, and message
+        cmd = """CREATE TABLE IF NOT EXISTS messages (
+                id INTEGER PRIMARY KEY,
+                handle TEXT,
+                message TEXT)"""
+        cursor = g.message_db.cursor()
+        cursor.execute(cmd)
+        return g.message_db
+    
+    
+def insert_message(request):
+    # extract message and handle from request
+    message = request.form['message']
+    handle = request.form['handle']
+    
+    # call the function get_message_db() to connect to the database
+    db = get_message_db()
+    
+    # get a cursor object
+    cursor = db.cursor()
+    
+    # get current number of row of the table 
+    cursor.execute("SELECT COUNT(*) FROM messages")
+    id_num = cursor.fetchone()[0] + 1
+    
+    # insert id, message, and handle into the table
+    cursor.execute("INSERT INTO messages (id, handle, message) VALUES (?, ?, ?)", (id_num, handle, message))
+    db.commit()
+    
+    # close the connection
+    db.close()
+    
+    return message, handle
 
-            # encode the PNG image to base64 string
-            pngImageB64String = "data:image/png;base64,"
-            pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
 
-            return render_template('submit.html',
-             image=pngImageB64String, digit=digit)
-        except:
-            return render_template('submit.html', error=True)
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+def random_messages(n):
+    # call the function get_message_db() to connect to the database
+    db = get_message_db()
+    
+    # get a cursor object
+    cursor = db.cursor()
+    
+    # get n random messages from the table
+    cursor.execute("SELECT * FROM messages ORDER BY RANDOM() LIMIT ?",(n,))
+    
+    # get all the messages 
+    messages = cursor.fetchall()
+    db.close()
+    
+    return messages
+    
